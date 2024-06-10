491337421506a0db19902633225924abd9f7d74c
Alex Colville <me@alexcolville.dev>
2024-06-10 15:29:25 +0100

    feat: setup structure for docs site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,57 +1,69 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'SRO Docusaurus Template',
-  tagline: 'A Docusaurus template for Southampton Robotics Outreach projects.',
-  favicon: 'img/favicon.ico',
+    title: "Smallpeice CER Docs",
+    tagline: "Student documentation for the Smallpeice CER summer school.",
+    favicon: "img/favicon.ico",
 
-  url: 'https://docusaurus-template.roboticsoutreach.org/',
-  baseUrl: '/',
+    url: "https://sp.roboticsoutreach.org/",
+    baseUrl: "/",
 
-  // GitHub pages deployment config.
-  organizationName: 'roboticsoutreach',
-  projectName: 'docusaurus-template',
+    // GitHub pages deployment config.
+    organizationName: "roboticsoutreach",
+    projectName: "smallpeice-docs",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
-        }
-      } satisfies Preset.Options,
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    sidebarPath: "./sidebars.ts",
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    navbar: {
-      title: 'SRO Docusaurus Template',
-      logo: {
-        alt: 'SRO Logo',
-        src: 'img/logo.svg',
-      },
-      items: [],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `Made with love by volunteers from <a href="https://roboticsoutreach.org">Southampton Robotics Outreach</a>. Built with <a href="https://docusaurus.io/">Docusaurus</a>.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    themeConfig: {
+        navbar: {
+            title: "Smallpeice CER Docs",
+            logo: {
+                alt: "SRO Logo",
+                src: "img/logo.svg",
+            },
+            items: [
+                {
+                    href: "/rules",
+                    position: "left",
+                    label: "Rules",
+                },
+                {
+                    type: "docSidebar",
+                    sidebarId: "docsSidebar",
+                    position: "left",
+                    label: "Docs",
+                },
+            ],
+        },
+        footer: {
+            style: "dark",
+            copyright: `Made with love by volunteers from <a href="https://roboticsoutreach.org">Southampton Robotics Outreach</a>. Built with <a href="https://docusaurus.io/">Docusaurus</a>.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
+
