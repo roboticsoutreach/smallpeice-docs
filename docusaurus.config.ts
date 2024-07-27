641baa193767ed5a271deb79ba32b1403d54b05d
WillB97 <will@rudi.org.uk>
2024-07-27 21:09:45 +0100

    Add remark-deflist to allow using definition lists

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import remarkDefList from 'remark-deflist';
 
 const config: Config = {
     title: "Smallpeice CER Docs",
@@ -28,6 +29,9 @@ const config: Config = {
             {
                 docs: {
                     sidebarPath: "./sidebars.ts",
+                    remarkPlugins: [
+                        remarkDefList,
+                    ],
                 },
             } satisfies Preset.Options,
         ],
