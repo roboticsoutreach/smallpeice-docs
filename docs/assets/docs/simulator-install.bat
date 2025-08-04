297d5544ea4d647b02a2f3d387f31b3c93df3958
WillB97 <will@rudi.org.uk>
2025-08-04 14:32:04 +0100

    Update sim install script

--- docs/assets/docs/simulator-install.bat
+++ docs/assets/docs/simulator-install.bat
@@ -1,4 +1,4 @@
 wget https://github.com/sourcebots/sbot_simulator/releases/download/2025.1.2/sbot-simulator-2025.1.2.zip
 unzip -d sbot-simulator/ sbot-simulator-2025.1.2.zip
 cd sbot-simulator/
-C:\Apps\Python311\python.exe setup.py
+"C:\Program Files\Python311\python.exe" setup.py
