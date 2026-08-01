914e6d5aed23e39b388339b8ce5123cd42624889
WillB97 <will@rudi.org.uk>
2026-08-01 17:14:16 +0100

    Initial script update

--- docs/assets/docs/simulator-install.bat
+++ docs/assets/docs/simulator-install.bat
@@ -1,4 +1,4 @@
-wget https://github.com/sourcebots/sbot_simulator/releases/download/2025.1.2/sbot-simulator-2025.1.2.zip
-unzip -d sbot-simulator/ sbot-simulator-2025.1.2.zip
+wget https://github.com/sourcebots/sbot_simulator/releases/download/2026.1.0/sbot-simulator-2026.1.0.zip
+unzip -d sbot-simulator/ sbot-simulator-2026.1.0.zip
 cd sbot-simulator/
-"C:\Program Files\Python311\python.exe" setup.py
+"C:\Program Files\Python313\python.exe" setup.py
