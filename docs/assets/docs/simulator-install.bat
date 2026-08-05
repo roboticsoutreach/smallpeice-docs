07aef96d7bf6e6a55c025a15831dcf7f1e3b2652
George Peppard <g.peppard@soton.ac.uk>
2026-08-05 13:56:05 +0100

    fix(simulator-install): python.exe is on the path

--- docs/assets/docs/simulator-install.bat
+++ docs/assets/docs/simulator-install.bat
@@ -1,4 +1,4 @@
 wget https://github.com/sourcebots/sbot_simulator/releases/download/2026.1.0/sbot-simulator-2026.1.0.zip
 unzip -d sbot-simulator/ sbot-simulator-2026.1.0.zip
 cd sbot-simulator/
-"C:\Program Files\Python313\python.exe" setup.py
+python setup.py
